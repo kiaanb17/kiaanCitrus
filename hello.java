705c3689369fe9ab606f48ba2304f4930354745f
kiaanb17 <kiaanb17@outlook.com>
2025-07-21 10:37:06 -0700

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,8 +1,21 @@
-public class hello {
+public class Hello {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static double trapazoid(double a, double b, double height) {
+
+        double area;
+
+        double s1 = a + b;
+        double s2 = s1/2;
+        area = s2*height;
+
+        return area;
+
     }
 
-    // this is practice
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(trapazoid(10,20,30));
+    }
+
 }
+
